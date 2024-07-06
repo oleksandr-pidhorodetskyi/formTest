83e17a2dd6c1cb9fed77135646bb350834521a8c
APidhorodetskyi <s.pidgorodetsky@gmail.com>
2024-07-06 16:10:58 +0200

    fix: remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const { colors: defaultColors } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
